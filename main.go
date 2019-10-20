4a8c898044afe802af89919bb62e4cd71f6c02ff
Adam Scholey <hello@adamjs.net>
2019-10-20 18:35:00 +0100

    Add response writer for standardised JSON payloads

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+// responsePayload represents a standardised JSON payload returned by API
+type responsePayload struct {
+	Status string      `json:"status"`
+	Data   interface{} `json:"data"`
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world")
+		payload := responsePayload{
+			Status: "ok",
+			Data: map[string]string{
+				"hello": "world",
+			},
+		}
+
+		writeResponse(w, payload, http.StatusOK)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func writeResponse(w http.ResponseWriter, p responsePayload, code int) {
+	// parse response payload
+	body, _ := json.Marshal(p)
+
+	// set headers
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	// write body
+	fmt.Fprintf(w, string(body))
 }

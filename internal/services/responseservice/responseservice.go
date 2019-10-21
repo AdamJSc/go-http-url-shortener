1337161893495741ab2e76634362e5eac03b1004
Adam Scholey <hello@adamjs.net>
2019-10-21 20:48:54 +0100

    Refactor response writers.
    Implement JSONResponse type, and move creation of new ok and error responses to Response Service.

--- internal/services/responseservice/responseservice.go
+++ internal/services/responseservice/responseservice.go
@@ -1,15 +1,71 @@
 package responseservice
 
-// Payload represents a standardised JSON payload returned by API
-type Payload struct {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// payload represents response body
+type payload struct {
 	Status string      `json:"status"`
 	Data   interface{} `json:"data"`
 }
 
-// NewPayload returns a new ResponsePayload
-func NewPayload(s string, d interface{}) Payload {
-	return Payload{
-		Status: s,
-		Data:   d,
+// JSONResponse represents status code and payload of a response
+type JSONResponse struct {
+	payload    payload
+	statusCode int
+}
+
+func (r JSONResponse) Write(w http.ResponseWriter) JSONResponse {
+	payload := r.payload
+	statusCode := r.statusCode
+
+	// set default http status
+	if statusCode == 0 {
+		statusCode = http.StatusInternalServerError
 	}
+
+	// parse response payload
+	body, _ := json.Marshal(payload)
+
+	// set headers
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	// write body
+	fmt.Fprintf(w, string(body))
+
+	return r
+}
+
+// NewOkResponse returns a new JSONResponse representing a successful request
+func NewOkResponse(data interface{}) JSONResponse {
+	r := JSONResponse{
+		payload: payload{
+			Status: "ok",
+			Data:   data,
+		},
+		statusCode: http.StatusOK,
+	}
+
+	return r
+}
+
+// NewErrResponse returns a new JSONResponse representing a request that generated an error
+func NewErrResponse(data interface{}, code ...int) JSONResponse {
+	r := JSONResponse{
+		payload: payload{
+			Status: "err",
+			Data:   data,
+		},
+	}
+
+	// set status code if provided to function call
+	if len(code) > 0 {
+		r.statusCode = code[0]
+	}
+
+	return r
 }
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"http-url-shortener/internal/services/responseservice"
 	"log"
 	"net/http"
@@ -14,40 +12,8 @@ func main() {
 			"hello": "world",
 		}
 
-		writeOkResponse(w, data)
+		responseservice.NewOkResponse(data).Write(w)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-// writeOk writes a standardised JSON response for a successful request
-func writeOkResponse(w http.ResponseWriter, data interface{}) {
-	payload := responseservice.NewPayload("ok", data)
-
-	writeResponse(w, payload, http.StatusOK)
-}
-
-// writeErr writes a standardised JSON response for a failed request
-func writeErrResponse(w http.ResponseWriter, data interface{}, code ...int) {
-	payload := responseservice.NewPayload("err", data)
-
-	// default to 500 unless an alternative code has been supplied
-	statusCode := http.StatusInternalServerError
-	if len(code) != 0 {
-		statusCode = code[0]
-	}
-
-	writeResponse(w, payload, statusCode)
-}
-
-func writeResponse(w http.ResponseWriter, p responseservice.Payload, code int) {
-	// parse response payload
-	body, _ := json.Marshal(p)
-
-	// set headers
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-
-	// write body
-	fmt.Fprintf(w, string(body))
 }

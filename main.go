4d1ce50f3d1f54227df64a0c477aae8a197ea803
Adam Scholey <hello@adamjs.net>
2019-10-23 20:47:47 +0100

    Assign handler function closure to variable

--- main.go
+++ main.go
@@ -9,29 +9,31 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", apiHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
 	workdir, _ := os.Getwd()
 	repository := shortenedurlfilesystemrepository.New(workdir + "/data")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// shortener endpoint
-		if r.URL.Path == "/api/shorten" {
-			if r.Method != "POST" {
-				w.WriteHeader(http.StatusMethodNotAllowed)
-				return
-			}
-
-			handlers.PostShorten(repository, w, r).Write(w)
-			return
-		}
-
-		// try to redirect a short URL
-		if r.Method != "GET" {
+	// shortener endpoint
+	if r.URL.Path == "/api/shorten" {
+		if r.Method != "POST" {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
 
-		handlers.GetShortURLRedirect(repository, w, r).Write(w)
-	})
+		handlers.PostShorten(repository, w, r).Write(w)
+		return
+	}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// try to redirect a short URL
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	handlers.GetShortURLRedirect(repository, w, r).Write(w)
 }
